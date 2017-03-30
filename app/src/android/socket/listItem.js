8510490d007344b66a33fcee86288007c5df46e3
coolworld2015 <wintermute2015@ukr.net>
2017-03-30 18:38:36 +0300

    [socket] - updated ListItem

--- app/src/android/socket/listItem.js
+++ app/src/android/socket/listItem.js
@@ -18,52 +18,100 @@ class ListItem extends Component {
 
     render() {
 		if (this.props.item.message != 'still alive') {
-			return (
-				<View style={{
-					margin: 10,
-					marginTop: 10,
-					backgroundColor: '#48BBEC',
-					//borderColor: '#48BBEC',
-					borderColor: '#48BBEC',
-					borderRadius: 25,
-					borderWidth: 5
-				}}>
-					<Text style={{		
-						fontSize: 20,
-						textAlign: 'center',
+			if (this.props.item.name == appConfig.socket.name) {
+				return (
+					<View style={{
 						margin: 10,
-						marginTop: 0,
+						marginTop: 10,
 						backgroundColor: '#48BBEC',
-						width: this.state.width * .85,
-						color: 'white',
-						fontWeight: 'bold'
+						//borderColor: '#48BBEC',
+						borderColor: '#48BBEC',
+						borderRadius: 25,
+						borderWidth: 5
 					}}>
-						{this.props.item.message}
-					</Text>
-					<Text style={{		
-						fontSize: 10,
-						textAlign: 'center',
-						marginTop: -5,
-						marginLeft: 10,
-						backgroundColor: '#48BBEC',
-						width: this.state.width * .85,
-						color: 'white'
+						<Text style={{		
+							fontSize: 20,
+							textAlign: 'center',
+							margin: 10,
+							marginTop: 0,
+							backgroundColor: '#48BBEC',
+							width: this.state.width * .85,
+							color: 'white',
+							fontWeight: 'bold'
+						}}>
+							{this.props.item.message}
+						</Text>
+						<Text style={{		
+							fontSize: 10,
+							textAlign: 'center',
+							marginTop: -5,
+							marginLeft: 10,
+							backgroundColor: '#48BBEC',
+							width: this.state.width * .85,
+							color: 'white'
+						}}>
+							{this.props.item.date}
+						</Text>					
+						<Text style={{		
+							fontSize: 10,
+							textAlign: 'center',
+							marginTop: 0,
+							marginLeft: 10,
+							backgroundColor: '#48BBEC',
+							width: this.state.width * .85,
+							color: 'white'
+						}}>
+							{this.props.item.name}
+						</Text>
+					</View>
+				);
+			} else {
+				return (
+					<View style={{
+						margin: 10,
+						marginTop: 10,
+						backgroundColor: 'blue',
+						borderColor: 'blue',
+						borderRadius: 25,
+						borderWidth: 5
 					}}>
-						{this.props.item.date}
-					</Text>					
-					<Text style={{		
-						fontSize: 10,
-						textAlign: 'center',
-						marginTop: 0,
-						marginLeft: 10,
-						backgroundColor: '#48BBEC',
-						width: this.state.width * .85,
-						color: 'white'
-					}}>
-						{this.props.item.name}
-					</Text>
-				</View>
-			);
+						<Text style={{		
+							fontSize: 20,
+							textAlign: 'center',
+							margin: 10,
+							marginTop: 0,
+							backgroundColor: 'blue',
+							width: this.state.width * .85,
+							color: 'white',
+							fontWeight: 'bold'
+						}}>
+							{this.props.item.message}
+						</Text>
+						<Text style={{		
+							fontSize: 10,
+							textAlign: 'center',
+							marginTop: -5,
+							marginLeft: 10,
+							backgroundColor: 'blue',
+							width: this.state.width * .85,
+							color: 'white'
+						}}>
+							{this.props.item.date}
+						</Text>					
+						<Text style={{		
+							fontSize: 10,
+							textAlign: 'center',
+							marginTop: 0,
+							marginLeft: 10,
+							backgroundColor: 'blue',
+							width: this.state.width * .85,
+							color: 'white'
+						}}>
+							{this.props.item.name}
+						</Text>
+					</View>
+				);				
+			}
 		} else {
 			return null;
 		}
